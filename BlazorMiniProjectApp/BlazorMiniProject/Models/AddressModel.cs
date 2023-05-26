c34ac0876d37b3760f76c7143a0bee05884b9d36
Lingling-Z <97594629+Lingling-Z@users.noreply.github.com>
2023-05-26 12:06:30 +1200

    initial commit

---
+++ BlazorMiniProjectApp/BlazorMiniProject/Models/AddressModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorMiniProject.Models
+{
+    public class AddressModel
+    {
+        public string Street { get; set; }
+        public string Suburb { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+
+    }
+}
